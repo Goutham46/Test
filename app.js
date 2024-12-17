df58f149792678fec3dd03c56a4d0094483233aa
Saiteja Vemula <saitejavemula@gmail.com>
2024-12-18 00:15:00 +0530

    Created new helper function - addTwoNumbers

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let a = 5;
 let b = 6;
-console.log(a+b);
+console.log(a + b);
 
+function addTwoNumbers(a, b) {
+    return a + b;
+}
 
 function substractTwoNumbers(a, b) {
     return a - b;
 }
 
+console.log('a + b = ', addTwoNumbers(a, b));
 console.log('a - b = ', substractTwoNumbers(a, b));
